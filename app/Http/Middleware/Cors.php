9d354fad803973c7d5ec9741176def10fa83318b
Souvik3798 <souvikbarua693@gmail.com>
2024-10-16 21:38:15 +0530

    new kernel

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,7 @@ class Cors
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Accept, Authorization, X-Requested-With, Application');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Accept, Authorization, X-Requested-With');
 
         return $response;
     }
